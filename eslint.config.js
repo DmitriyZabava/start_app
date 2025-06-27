7af49d9a50820ff33bdf248ccaed061e143a656f
DmitriyZabava <zdw1978@gmail.com>
2025-06-27 22:54:43 +0300

    create starter app TS Webpack React SCSS

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,9 @@ const { defineConfig } = require("eslint/config");
 const globals = require("globals");
 
 const {
-	fixupConfigRules,
-	fixupPluginRules,
-	// eslint-disable-next-line
+    fixupConfigRules,
+    fixupPluginRules,
+    // eslint-disable-next-line
 } = require("@eslint/compat");
 // eslint-disable-next-line
 const tsParser = require("@typescript-eslint/parser");
@@ -23,114 +23,100 @@ const js = require("@eslint/js");
 const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
-	// eslint-disable-next-line
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
+    // eslint-disable-next-line
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
-module.exports = defineConfig([
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.jest,
-				__IS_DEV__: true,
-				module: true,
-			},
+module.exports = defineConfig([{
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.jest,
+            __IS_DEV__: true,
+            module: true,
+        },
 
-			parser: tsParser,
-			ecmaVersion: "latest",
-			sourceType: "module",
-			parserOptions: {
-				ecmaFeatures: {
-					jsx: true,
-				},
-			},
-		},
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
 
-		extends: fixupConfigRules(
-			compat.extends(
-				"eslint:recommended",
-				"plugin:@typescript-eslint/recommended",
-				"plugin:react/recommended",
-				"plugin:react-hooks/recommended"
-			)
-		),
+    extends: fixupConfigRules(compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+    )),
 
-		plugins: {
-			"@typescript-eslint": fixupPluginRules(typescriptEslint),
-			react: fixupPluginRules(react),
-			"react-hooks": fixupPluginRules(reactHooks),
-		},
+    plugins: {
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        react: fixupPluginRules(react),
+        "react-hooks": fixupPluginRules(reactHooks),
+    },
 
-		settings: {
-			react: {
-				version: "detect", // Автоматически определяет версию из `node_modules/react`
-			},
-		},
+    settings: {
+        react: {
+            version: "detect", // Автоматически определяет версию из `node_modules/react`
+        },
+    },
 
-		rules: {
-			quotes: ["error", "double"],
-			indent: [2, 4],
-			"@typescript-eslint/no-var-requires": 0,
-			"react/jsx-indent": [2, 4],
-			"react/jsx-indent-props": [2, 4],
+    rules: {
+        quotes: ["error", "double"],
+        indent: [2, 4],
+        "@typescript-eslint/no-var-requires": 0,
+        "react/jsx-indent": [2, 4],
+        "react/jsx-indent-props": [2, 4],
 
-			"react/jsx-filename-extension": [
-				2,
-				{
-					extensions: [".js", ".jsx", ".tsx"],
-				},
-			],
-			"react/jsx-uses-react": "error",
-			"react/jsx-uses-vars": "error",
-			"react/require-default-props": "off",
-			"react/react-in-jsx-scope": "off",
-			"react/jsx-props-no-spreading": "warn",
-			"react/function-component-definition": "off",
-			"react/no-deprecated": "off",
-			"react/display-name": "off",
-			"react-hooks/rules-of-hooks": "error",
-			"react-hooks/exhaustive-deps": "error",
-			"import/no-unresolved": "off",
-			"import/prefer-default-export": "off",
-			"import/extensions": "off",
-			"import/no-extraneous-dependencies": "off",
-			"no-unused-vars": ["warn"],
-			"no-shadow": "off",
-			"no-underscore-dangle": "off",
-			"object-curly-spacing": ["error", "always"],
+        "react/jsx-filename-extension": [2, {
+            extensions: [".js", ".jsx", ".tsx"],
+        }],
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "react/require-default-props": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-props-no-spreading": "warn",
+        "react/function-component-definition": "off",
+        "react/no-deprecated": "off",
+        "react/display-name": "off",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "error",
+        "import/no-unresolved": "off",
+        "import/prefer-default-export": "off",
+        "import/extensions": "off",
+        "import/no-extraneous-dependencies": "off",
+        "no-unused-vars": ["warn"],
+        "no-shadow": "off",
+        "no-underscore-dangle": "off",
+        "object-curly-spacing": ["error", "always"],
 
-			"max-len": [
-				"error",
-				{
-					code: 100,
-					ignoreComments: true,
-				},
-			],
+        "max-len": ["error", {
+            code: 100,
+            ignoreComments: true,
+        }],
 
-			"jsx-ally/no-static-element-interactions": "off",
-			"jsx-ally/click-events-have-key-events": "off",
-		},
-	},
-	{
-		languageOptions: {
-			globals: {
-				...globals.node,
-			},
+        "jsx-ally/no-static-element-interactions": "off",
+        "jsx-ally/click-events-have-key-events": "off",
+    },
+}, {
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
 
-			sourceType: "script",
-			parserOptions: {},
-		},
+        sourceType: "script",
+        parserOptions: {},
+    },
 
-		files: [
-			"**/src/**/*.{test,stories}.{ts,tsx}",
-			"**/.eslintrc.{js,cjs}",
-		],
+    files: ["**/src/**/*.{test,stories}.{ts,tsx}", "**/.eslintrc.{js,cjs}"],
 
-		rules: {
-			"max-len": "off",
-		},
-	},
-]);
+    rules: {
+        "max-len": "off",
+    },
+}]);
